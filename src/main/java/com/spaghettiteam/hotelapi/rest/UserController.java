5cc117edb40e211bd9a5dac45d3e808283ff1a0c
Vengardd <33378090+Vengardd@users.noreply.github.com>
2019-02-22 16:42:21 +0100

    added crossorigins to all controllers (#4)

--- src/main/java/com/spaghettiteam/hotelapi/rest/UserController.java
+++ src/main/java/com/spaghettiteam/hotelapi/rest/UserController.java
@@ -3,11 +3,13 @@ package com.spaghettiteam.hotelapi.rest;
 import com.spaghettiteam.hotelapi.model.User;
 import com.spaghettiteam.hotelapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
